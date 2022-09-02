5eebe432ff4b414b129edec4cc2ce373fdaa38b7
Luis Bertel <ellipe@gmail.com>
2022-09-02 13:17:57 -0500

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Pets.Repo
+alias Pets.Companion.Pet
+
+Repo.insert!(%Pet{name: "Chiquita", breed: "Pinchee", gender: "Female", birthdate: ~D[2003-05-01]})
+
+Repo.insert!(%Pet{
+  name: "Canela",
+  breed: "Jack Russel Terrier",
+  gender: "Female",
+  birthdate: ~D[2003-05-01]
+})
+
+Repo.insert!(%Pet{
+  name: "Macarena",
+  breed: "Jack Russel Terrier",
+  gender: "Female",
+  birthdate: ~D[2003-05-01]
+})
